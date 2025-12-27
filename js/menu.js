9bc9f08d8ecef2c5f56d853051d3dae3f2116c1f
okawa0 <apple_ui@icloud.com>
2025-12-27 17:02:44 +0900

    copyright追加

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,3 @@
-// // ハンバーガーメニュー
-// // ハンバーガーボタンの要素取得
-// const btn = document.querySelector('.hamburger-btn');
-// // メニューの要素取得
-// const menu = document.querySelector('.global-nav');
-
-// // クリックイベントの開始
-// btn.addEventListener('click', () => {
-// 	// メニューが開いているかを取得
-// 	const isOpen = btn.classList.toggle('is-active');
-// 	menu.classList.toggle('is-open',isOpen);
-// });
-
 // ハンバーガーメニュー
 document.querySelectorAll('.hamburger-btn').forEach(btn => {
   const menuId = btn.getAttribute('aria-controls');
